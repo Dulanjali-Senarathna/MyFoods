32f6ab00791514a2f4f8866a385fc397d003c1b3
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-06-27 13:28:17 +0530

    Fetching data from PHP MySQL API using http package Done

--- lib/src/scoped-model/food_model.dart
+++ lib/src/scoped-model/food_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:scoped_model/scoped_model.dart';
 import 'package:MyFoods/src/models/food_model.dart';
 import 'package:http/http.dart' as http;
@@ -20,7 +22,24 @@ class FoodModel extends Model
   {
     http.get("http://192.168.8.101/flutter_food_app/api/foods/getFoods.php").then((http.Response response)
     {
-      print("Fetching data: ${response.body}");
+     // print("Fetching data: ${response.body}");
+     final List<Food> fetchedFoodItems = [];
+      final List fetchedData = json.decode(response.body);
+      print(fetchedData);
+      fetchedData.forEach((data) {
+        Food food = Food(
+          id: data["id"],
+          category: data["category"],
+          discount: double.parse(data["discount"]),
+          imagePath: data["imagePath"],
+          name: data["name"],
+          price: double.parse(data["price"]),
+        );
+        fetchedFoodItems.add(food);
+      });
+      _foods = fetchedFoodItems;
+      print(_foods);
     });
+    
   }
 }
