5115ae5eec54b937f8624a8beeba34d8735dc25c
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-06-29 07:11:16 +0530

    Validating and saving data from the text form fields Done

--- lib/src/models/food_model.dart
+++ lib/src/models/food_model.dart
@@ -1,7 +1,7 @@
 class Food
 {
-final String id, name, category, imagePath;
+final String id, name, category, imagePath, description;
 final double price, discount, ratings;
 
-Food ({this.id, this.name, this.category, this.imagePath, this.price, this.discount,this.ratings});
+Food ({this.id, this.name, this.category, this.imagePath,this.description, this.price, this.discount,this.ratings});
 }
