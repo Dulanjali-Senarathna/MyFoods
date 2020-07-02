6a9356ae8cef16e970b9895c0d8f74e660488024
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-07-02 15:46:03 +0530

    Authentication, signing up a user Done

---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,10 @@
+class User
+{
+  final String username,firstName,lastName,email,phoneNumber,userType;
+
+  User(
+    {
+      this.username,this.firstName,this.lastName,this.email,this.phoneNumber,this.userType
+    }
+  );
+}
--- lib/src/pages/signup_page.dart
+++ lib/src/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:MyFoods/src/widgets/button.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 import '../pages/signin_page.dart';
 
 class SignUpPage extends StatefulWidget
@@ -11,11 +12,15 @@ class SignUpPage extends StatefulWidget
 class _SignUpPageState extends State<SignUpPage>{
 
   bool _toggleVisibility = true;
-  bool _toggleConfirmVisibility = true;
+  //bool _toggleConfirmVisibility = true;
+
+  String _email, _password,_username;
+
+  GlobalKey <FormState> _formKey  = GlobalKey();
 
   Widget _buildEmailTextField()
   {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         hintText:"Email",
         hintStyle: TextStyle(
@@ -24,12 +29,29 @@ class _SignUpPageState extends State<SignUpPage>{
 
         )
       ),
+      onSaved: (String email)
+      {
+        _email = email;
+      },
+      validator: (String email)
+      {
+        String errorMessage;
+        if(!email.contains("@"))
+        {
+        errorMessage = "Your email is incorrect";
+        }
+        if(email.isEmpty)
+        {
+          errorMessage = "Email is required";
+        }
+        return errorMessage;
+      },
     );
   }
 
   Widget _buildUsernameTextField()
   {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         hintText:"Username",
         hintStyle: TextStyle(
@@ -38,13 +60,26 @@ class _SignUpPageState extends State<SignUpPage>{
 
         )
       ),
+      onSaved: (String username)
+      {
+        _username = username.trim();
+      },
+      validator: (String username)
+      {
+        String errorMessage;
+        if(username.isEmpty)
+        {
+          errorMessage = "User name field is required";
+        }
+        return errorMessage;
+      },
     );
   }
 
   
   Widget _buildPasswordTextField()
   {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         hintText:"Password",
         hintStyle: TextStyle(
@@ -62,87 +97,147 @@ class _SignUpPageState extends State<SignUpPage>{
         ),
       ),
       obscureText: _toggleVisibility,
+     onSaved: (String password)
+     {
+       _password = password;
+     },
+     validator: (String password)
+     {
+        String errorMessage;
+        if(password.isEmpty)
+        {
+          errorMessage = "Password is required";
+        }
+        return errorMessage;
+     },
     );
   }
 
-  Widget _buildConfirmPasswordTextField()
-  {
-    return TextField(
-      decoration: InputDecoration(
-        hintText:"Confirm Password",
-        hintStyle: TextStyle(
-          color: Color(0xFFBDC2CB),
-          fontSize: 18.0,
-        ),
-        suffixIcon: IconButton(
-          onPressed: ()
-          {
-            setState(() {
-              _toggleConfirmVisibility = !_toggleConfirmVisibility;
-            });
-          },
-          icon:_toggleConfirmVisibility ? Icon(Icons.visibility_off) : Icon(Icons.visibility) ,
-        ),
-      ),
-      obscureText:_toggleConfirmVisibility,
-    );
-  }
+  // Widget _buildConfirmPasswordTextField()
+  // {
+  //   return TextFormField(
+  //     decoration: InputDecoration(
+  //       hintText:"Confirm Password",
+  //       hintStyle: TextStyle(
+  //         color: Color(0xFFBDC2CB),
+  //         fontSize: 18.0,
+  //       ),
+  //       suffixIcon: IconButton(
+  //         onPressed: ()
+  //         {
+  //           setState(() {
+  //             _toggleConfirmVisibility = !_toggleConfirmVisibility;
+  //           });
+  //         },
+  //         icon:_toggleConfirmVisibility ? Icon(Icons.visibility_off) : Icon(Icons.visibility) ,
+  //       ),
+  //     ),
+  //     obscureText:_toggleConfirmVisibility,
+  //      onSaved:( String value)
+  //     {
+
+  //     },
+  //   );
+  // }
   @override
   Widget build(BuildContext context)
   {
-    return Scaffold(
-      resizeToAvoidBottomPadding:false,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal:10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            Text(
-              "Sign Up", 
-              style: TextStyle(
-                fontSize: 40.0, 
-                fontWeight: FontWeight.bold,
-                ),
-                ),
-                SizedBox(height:50.0),
-              Card(
-                  elevation: 5.0,
-                  child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                      child: Column(
-                      children: <Widget>[
-                         _buildUsernameTextField(),
-                          SizedBox(height:20.0),
-                        _buildEmailTextField(),
-                        SizedBox(height:20.0),
-                        _buildPasswordTextField(),
-                         SizedBox(height:20.0),
-                          _buildConfirmPasswordTextField(),
-                      ]
+    return SafeArea(
+          child: Scaffold(
+        resizeToAvoidBottomPadding:false,
+        backgroundColor: Colors.grey.shade100,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal:10.0),
+          child: Form(
+            key: _formKey,
+                    child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Text(
+                  "Sign Up", 
+                  style: TextStyle(
+                    fontSize: 40.0, 
+                    fontWeight: FontWeight.bold,
                     ),
-                  ),
-                ),
-                SizedBox(height:30.0),
-                Button(btnText:"Sign Up"),
-                Divider(height: 20.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children :<Widget>[
-                    Text("Allready have an account?",  style: TextStyle(fontSize:18.0, color:Color(0xFFBDC2CB), fontWeight:FontWeight.bold),),
-                    SizedBox(width:10.0),
-                    GestureDetector( 
-                      onTap:(){
-                         Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context ) => SignInPage()));
-                      },
-                      child: Text(
-                      "Sign In",  style: TextStyle(fontSize:18.0, color:Colors.blueAccent, fontWeight:FontWeight.bold),
+                    ),
+                    SizedBox(height:50.0),
+                  Card(
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                          child: Column(
+                          children: <Widget>[
+                             _buildUsernameTextField(),
+                              SizedBox(height:20.0),
+                            _buildEmailTextField(),
+                            SizedBox(height:20.0),
+                            _buildPasswordTextField(),
+                          ]
+                        ),
                       ),
-                      ),
-                  ],
-                ),
-          ],
+                    ),
+                    SizedBox(height:30.0),
+                    _buildSignUpButton(),
+                    Divider(height: 20.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children :<Widget>[
+                        Text("Allready have an account?",  style: TextStyle(fontSize:18.0, color:Color(0xFFBDC2CB), fontWeight:FontWeight.bold),),
+                        SizedBox(width:10.0),
+                        GestureDetector( 
+                          onTap:(){
+                             Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context ) => SignInPage()));
+                          },
+                          child: Text(
+                          "Sign In",  style: TextStyle(fontSize:18.0, color:Colors.blueAccent, fontWeight:FontWeight.bold),
+                          ),
+                          ),
+                      ],
+                    ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+  Widget _buildSignUpButton()
+  {
+    return ScopedModelDescendant(
+      builder: (BuildContext sctx, Widget child, MainModel model)
+      {
+        return GestureDetector(
+      onTap: ()
+      {
+        onSubmit(model.authenticate);
+      },
+          child: Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Center(
+                      child:Text("Sign In",
+                      style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                      ),),
+                    ),
+                  ),
+    );
+      }
+    );
+  }
+  void onSubmit(Function authenticate)
+  {
+    if(_formKey.currentState.validate())
+    {
+      _formKey.currentState.save();
+
+      print("The email : $_email, The password: $_password");
+      authenticate(_email,_password);
+    }
   }
 }
